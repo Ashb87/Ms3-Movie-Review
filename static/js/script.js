a244110280bab65ee9885761706e9bcf5f7cf4e0
Ashb87 <ashleyb182@hotmail.co.uk>
2021-10-05 11:43:34 +0000

    add register template

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
     $(".sidenav").sidenav();
   });
+
+
+
+
+  const checkIfPasswordsMatch = () => {
+    let password = document.getElementById('password');
+    let confirmPassword = document.getElementById('confirm_password');
+  
+    let messageText = document.querySelector('.message-text');
+    let regButton = document.querySelector('.btn-large');
+  
+    // Check if passwords match
+  
+    if (password.value !== confirmPassword.value) {
+      messageText.innerHTML = 'Passwords do not match!';
+      messageText.style.color = 'red';
+  
+      regButton.setAttribute('disabled', true);
+      regButton.classList.add('btn-disabled');
+    } else if (password.value === confirmPassword.value) {
+      messageText.innerHTML = 'Passwords match!';
+      messageText.style.color = 'green';
+      
+      regButton.removeAttribute('disabled', true);
+      regButton.classList.remove('btn-disabled');
+    }
+  };
